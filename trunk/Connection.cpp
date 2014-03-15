5c8677f2eb2563b784e4d0b71f65b6f3fff9d4cd
TheNawaKer <jimmy.furet@gmail.com>
2014-03-15 13:34:02 +0100

    scan planete

--- trunk/Connection.cpp
+++ trunk/Connection.cpp
@@ -18,13 +18,17 @@ cout<<"start"<<endl;
 ia.set_session(&session);
 if(session.waitInit()){
 	cout<<"game started"<<endl;
+	bool t0=true;
 	while(true){
 		session.waitRoundStarting();
 		GameData gd = session.gameData();
 		ia.read_data(gd);
+		if(t0){
+			ia.get_distances();
+			t0=false;
+		}
 		ia.choose();
 		session.sendOrders();
-
 	}
 }else{
 	throw session.lastError().c_str();
