8c6da0a4e12a4b1bccec23c524fc80ce90ec89d7
MonPC <monpc@ubuntu.(none)>
2014-03-15 11:09:09 +0100

    c

---
+++ trunk/IA1_0/IA.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+#define List vector
+#define String string
+typedef List<Planet> PlanetList;
+typedef List<ScanResult> ScanResultList;
+typedef List<Fleet> FleetList;
+typedef List<Ennemy> EnnemyList;
+typedef List<FightReport> FightReportList;
