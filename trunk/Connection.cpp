75c1ec665cf77649fbc87a6e6576720663a71d02
TheNawaKer <jimmy.furet@gmail.com>
2014-03-15 11:53:20 +0100

    version sans ia

--- trunk/Connection.cpp
+++ trunk/Connection.cpp
@@ -13,10 +13,10 @@ Connection::Connection(string ip,int port , string pseudo){
 	}
 }
 
-Connection::start(IA & ia){
+Connection::start(/*IA & ia*/){
 	if(session.waitInit()){
 		while(session.waitRoundStarting()){
-			ia.readData(session.gameData());
+			//ia.readData(session.gameData());
 		}
 	}else{
 		throw session.lastError().c_str();
--- trunk/Connection.hpp
+++ trunk/Connection.hpp
@@ -10,7 +10,7 @@ private:
 	Session session;
 public:
 	Connection(std::string ip,int port,std::string pseudo);
-	start(IA & a);
+	start(/*IA & a*/);
 	~Connection();
 };
 
