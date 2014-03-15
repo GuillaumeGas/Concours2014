2478319ee7884bd8ae0339f5744c10a0bf1c4fcb
GuillaumeGas <gas.guillaume@free.fr>
2014-03-15 11:44:16 +0100

    modif pseudo

--- trunk/main.cpp
+++ trunk/main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(){
 	try{
-		Connection("127.0.0.1",4242,"*team->power(socket&)");	
+		Connection("127.0.0.1",4242,"test");	
 	}catch(const char * e){
 		cerr<<e<<endl;
 		return -1;
